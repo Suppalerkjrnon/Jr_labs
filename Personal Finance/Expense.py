f403b2060ead7f4d1ac4cfbf70050e5e801178d6
Suppalerkjrnon <suppalerkjrnon@gmail.com>
2024-05-14 22:43:36 +0700

    ๊Update the overide logic

--- Personal Finance/Expense.py
+++ Personal Finance/Expense.py
@@ -4,23 +4,28 @@ import streamlit as st
 class Expense:
     def __init__(self):
         # Read the expense and service data
-        expense = pd.read_csv('/media/dr01/work/jovyan/work/Misc/Junior Lab/Fun project/Personal_Finance/pages/asset/Expense File Template - expense.csv')
+        expense = pd.read_csv('F:\\For work\\Personal Finance\\pages\\asset\\Expense File Template - expense.csv')
+        #Display expense data as a dataframe
+        self.df = pd.DataFrame(expense)
+        self.total_df = pd.DataFrame({'cost': self.df['cost'].sum()}, index=['Total'])
+        self.total_df = pd.concat([self.df, self.total_df])
         
-        # Display service data by using service_checkbox
-        #Populate checkbox by using variable in expense data as a label, using st.multiselect
+        #Last row in the dataframe, column expense write string 'Total'
+        self.total_df.loc['Total', 'expense'] = 'Total Expense'
+
+        #Create a checkbox to allow user to select expenses to overide
         self.expense_overide = st.checkbox('Expense Overide')
         
+        #If the checkbox is ticked, display a multiselect widget to allow user to select expenses to overide
         if self.expense_overide:
             self.expense_list = st.multiselect('Expense Override', expense['expense'])
-            
+            #If the user has selected expenses to overide, display a success message
             if self.expense_list:
                 self.success = st.success(f"Selected to Override {len(self.expense_list)} expenses")
                 
                 self.edit_df_list = []
                 self.overide_list = []
-                
                 selected_expenses_df = pd.DataFrame()  # Initialize an empty DataFrame to store selected expenses
-                
                 # Concatenate selected expense rows into a single DataFrame
                 for expense_item in self.expense_list:
                     expense_row = expense[expense['expense'] == expense_item]
@@ -32,7 +37,17 @@ class Expense:
                 # Append the data editor widget for the selected expenses
                 unique_key = "selected_expenses_editor"  # Use a single key for the combined editor
                 self.edit_df_list.append(st.data_editor(selected_expenses_df, key=unique_key, num_rows='dynamic'))
+                #Make edit df list to be a dataframe
+                self.edit_df = pd.concat(self.edit_df_list)
                 
-        else: # Default behavior if no overide is selected
-            
-            st.dataframe(expense, width=1200)
+                # Going inside the data editor index and row to get the overide data
+                for self.index, self.row in self.edit_df.iterrows():
+                    self.df.loc[self.df['expense'] == self.row['expense'], 'cost'] = self.row['cost']
+                    
+                #Recalculate the total cost
+                self.total_df = pd.DataFrame({'cost': [self.df['cost'].sum()]}, index=['Total'])
+                self.total_df = pd.concat([self.df, self.total_df])
+                self.total_df.loc['Total', 'expense'] = 'Total Expense'
+                
+        # Default behavior if no overide is selected   
+        st.dataframe(self.total_df, width=1200)
