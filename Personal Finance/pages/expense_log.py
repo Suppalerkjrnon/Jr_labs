0ee1c4c2a43c9348a6424c79200fb90a3c4459a6
Suppalerkjrnon <suppalerkjrnon@gmail.com>
2024-06-01 15:05:11 +0700

    Visualize the first month expense report and generate graph

--- Personal Finance/pages/expense_log.py
+++ Personal Finance/pages/expense_log.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import plotly.express as px
 
 class Expense_Log:
     def __init__(self):
@@ -11,10 +12,8 @@ class Expense_Log:
             self.existing_data = self.conn.read(spreadsheet= 'https://docs.google.com/spreadsheets/d/1D3sD13BEx8B7HEr-wSsZPlqB_9YIH0LFDbOmMoaoLhY/edit?usp=sharing')
             # Convert data to DataFrame
             self.df = pd.DataFrame(self.existing_data)
-            
-            last_row = pd.DataFrame({'cost': self.df['cost'].sum()}, index=['Total'])
-            
-            self.total_df = pd.concat([self.df, last_row])
+            # last_row = pd.DataFrame({'cost': self.df['cost'].sum()}, index=['Total'])
+            # self.total_df = pd.concat([self.df, last_row])
             
         except Exception as e:
             st.error(f"Error: {e}")
@@ -25,7 +24,22 @@ def main():
     expense_log = Expense_Log()
     
     if hasattr(expense_log, 'df'):
-        st.dataframe(expense_log.total_df, width=1000)
+        if 'report' not in st.session_state:
+            st.session_state.report = False
+        
+        if not st.session_state.report:
+            st.dataframe(expense_log.df, width=1000)
+        
+        Report = st.button("Generate Report")
+        if Report:
+            st.session_state.report = True
+            # Summary the total cost
+            st.success(f"Total Expense: {expense_log.df['cost'].sum()}")
+            #Create source object for plot where expense_log.total_df value is not null
+            source = expense_log.df[expense_log.df['cost'].notnull()]
+            fig = px.pie(source, values='cost', names='type', title='Expense Distribution')
+            st.plotly_chart(fig)
+        
     else:
         st.error("Failed to load data.")
 
